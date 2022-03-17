7f8df4dca328999ef66b7134a90ea930e694fd1e
vharikar <vibha_harikar@intuit.com>
2022-03-17 02:51:27 +0000

    Added my data

--- app.py
+++ app.py
@@ -10,18 +10,17 @@ import plotly.graph_objs as go
 
 ###### Define your variables #####
 tabtitle = 'Titanic!'
-color1='#92A5E8'
-color2='#8E44AD'
-color3='#FFC300'
+color1='#007FFF'
+color2='#EFBBCC'
+color3='#FFFF99'
 sourceurl = 'https://www.kaggle.com/c/titanic'
-githublink = 'https://github.com/plotly-dash-apps/304-titanic-dropdown'
+githublink = 'https://github.com/vharkar/304-titanic-dropdown'
 
 
 ###### Import a dataframe #######
-df = pd.read_csv("https://raw.githubusercontent.com/austinlasseter/plotly_dash_tutorial/master/00%20resources/titanic.csv")
-df['Female']=df['Sex'].map({'male':0, 'female':1})
-df['Cabin Class'] = df['Pclass'].map({1:'first', 2: 'second', 3:'third'})
-variables_list=['Survived', 'Female', 'Fare', 'Age']
+df = pd.read_csv("assets/titanic_data.csv")
+variables_list=['Survived', 'AccompanyingParentsOrChildren', 'Fare', 'Age']
+values_list=['survived','parch','fare','age']
 
 ########### Initiate the app
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -34,8 +33,8 @@ app.layout = html.Div([
     html.H3('Choose a continuous variable for summary statistics:'),
     dcc.Dropdown(
         id='dropdown',
-        options=[{'label': i, 'value': i} for i in variables_list],
-        value=variables_list[0]
+        options=[{'label': variables_list[i], 'value': values_list[i]} for i in range(0, len(variables_list))],
+        value=values_list[0]
     ),
     html.Br(),
     dcc.Graph(id='display-value'),
@@ -49,25 +48,25 @@ app.layout = html.Div([
 @app.callback(Output('display-value', 'figure'),
               [Input('dropdown', 'value')])
 def display_value(continuous_var):
-    grouped_mean=df.groupby(['Cabin Class', 'Embarked'])[continuous_var].mean()
+    grouped_mean=df.groupby(['who', 'embark_town'])[continuous_var].mean()
     results=pd.DataFrame(grouped_mean)
     # Create a grouped bar chart
     mydata1 = go.Bar(
         x=results.loc['first'].index,
         y=results.loc['first'][continuous_var],
-        name='First Class',
+        name='man',
         marker=dict(color=color1)
     )
     mydata2 = go.Bar(
         x=results.loc['second'].index,
         y=results.loc['second'][continuous_var],
-        name='Second Class',
+        name='woman',
         marker=dict(color=color2)
     )
     mydata3 = go.Bar(
         x=results.loc['third'].index,
         y=results.loc['third'][continuous_var],
-        name='Third Class',
+        name='child',
         marker=dict(color=color3)
     )
 
