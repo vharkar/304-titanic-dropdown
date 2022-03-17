ff4d4379814250c6f4195578098d83160497e6a6
vharikar <vibha_harikar@intuit.com>
2022-03-17 03:49:30 +0000

    Fixed labels

--- app.py
+++ app.py
@@ -19,9 +19,14 @@ githublink = 'https://github.com/vharkar/304-titanic-dropdown'
 
 ###### Import a dataframe #######
 df = pd.read_csv("assets/titanic_data.csv")
+
 df=df[['survived','who','embark_town', 'age', 'fare', 'parch']]
-variables_list=['Survived', 'AccompanyingParentsOrChildren', 'Fare', 'Age']
-values_list=['survived','parch','fare','age']
+df.rename(columns={'parch': 'WithParentsOrChildren'}, inplace=True)
+df.rename(columns={'survived': 'Survived'}, inplace=True)
+df.rename(columns={'age': 'Age'}, inplace=True)
+df.rename(columns={'fare': 'Fare'}, inplace=True)
+
+variables_list=['Survived', 'WithParentsOrChildren', 'Fare', 'Age']
 
 ########### Initiate the app
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -34,7 +39,7 @@ app.layout = html.Div([
     html.H3('Choose an option for summary statistics:'),
     dcc.Dropdown(
         id='dropdown',
-        options=[{'label': variables_list[i], 'value': i} for i in range(0, len(variables_list))],
+        options=[{'label': i, 'value': i} for i in variables_list],
         value=0
     ),
     html.Br(),
@@ -48,34 +53,33 @@ app.layout = html.Div([
 ######### Interactive callbacks go here #########
 @app.callback(Output('display-value', 'figure'),
               [Input('dropdown', 'value')])
-def display_value(continuous_var_idx):
+def display_value(continuous_var):
     grouped_mean=df.groupby(['who', 'embark_town'])[continuous_var].mean()
     results=pd.DataFrame(grouped_mean)
     # Create a grouped bar chart
     
     mydata1 = go.Bar(
         x=results.loc['man'].index,
-        y=results.loc['man'][values_list[continuous_var_idx]],
+        y=results.loc['man'][continuous_var],
         name='man',
         marker=dict(color='#007FFF')
     )
     mydata2 = go.Bar(
         x=results.loc['woman'].index,
-        y=results.loc['woman'][values_list[continuous_var_idx]],
+        y=results.loc['woman'][continuous_var],
         name='woman',
         marker=dict(color='#EFBBCC')
     )
     mydata3 = go.Bar(
         x=results.loc['child'].index,
-        y=results.loc['child'][values_list[continuous_var_idx]],
+        y=results.loc['child'][continuous_var],
         name='child',
         marker=dict(color='#FFFF99')
     )
     mylayout = go.Layout(
         title='Grouped bar chart',
         xaxis = dict(title = 'Port of Embarkation'), # x-axis label
-        yaxis = dict(title = str(variables_list[continuous_var_idx])), # y-axis label
-
+        yaxis = dict(title = str(continuous_var)), # y-axis label
     )
 
     fig = go.Figure(data=[mydata1, mydata2, mydata3], layout=mylayout)
